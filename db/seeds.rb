7b9a39a2f39fa604cdfe3bbea767b98495d6c594
PC <hamlington890@gmail.com>
2021-02-03 13:21:01 -0500

    seeded data and added some associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+CSV.foreach(Rails.root.join("db/seeds_data/dog.csv"), headers: true) do |row|
+	Product.find_or_create_by(title: row[0], price: row[1], description: row[2], image_url: row[3])
+end
